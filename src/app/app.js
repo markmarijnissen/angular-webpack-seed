925f154534af03f2538c7550e15ae147c8fb269e
Mark Marijnissen <markmarijnissen@gmail.com>
2014-09-18 01:11:23 +0200

    controller hotloading and bugfixes

--- src/app/app.js
+++ src/app/app.js
@@ -1,30 +1,46 @@
 angular.module('app',[
   'ui.router'
 ])
-.constant('templates',require.context('./',true,/^\.\/.*\.state\.jade$/))
-.config(function($stateProvider,$urlRouterProvider,templates){
+.constant('require',(function(){
+  var TemplateRequire, ControllerRequire;
+  function load(){
+    TemplateRequire = require.context('./',true,/^\.\/.*\.state\.jade$/);
+    ControllerRequire = require.context('./',true,/^\.\/.*Ctrl$/);
+  }
+  load();
+  return {
+    ids: [TemplateRequire.id,ControllerRequire.id],
+    reload: load,
+    template: function(name,data){
+      return function(){
+        return TemplateRequire('./'+name+'.state.jade')(data);
+      };
+    },
+    controller: function(name){
+      return function(){
+        return ControllerRequire('./'+name);
+      };
+    }
+  };
+})())
+.config(function($stateProvider,$urlRouterProvider,require){
   $urlRouterProvider.otherwise('/a');
   $stateProvider
     .state('a',{
       url: "/a",
-      template: createTemplateFn('a')
+      template: require.template('a/a'),
+      controllerProvider: require.controller('a/ACtrl')
     })
     .state('b',{
       url: "/b",
-      template: createTemplateFn('b')
+      template: require.template('b/b'),
+      controllerProvider: require.controller('b/BCtrl')
     });
-
-  // createTemplate; 
-  // helper to wrap a function around a template require call
-  function createTemplateFn(name,data) {
-    return function(){
-      return templates('./'+name+'.state.jade')(data);
-    };
-  }
 })
-.run(function($state,templates){
+.run(function($state,require){
   if(module.hot){
-    module.hot.accept(templates.id,function(){
+    module.hot.accept(require.ids,function() {
+      require.reload();
       $state.transitionTo($state.current, $state.params, {
           reload: true,
           inherit: false,
@@ -33,3 +49,4 @@ angular.module('app',[
     });
   }
 });
+
